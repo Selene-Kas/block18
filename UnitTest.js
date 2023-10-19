a92121afe0928ef9b45bbaf3b9577be13655e6f1
Kassandra Medina <kassandramedina@Kassandras-MBP.lan>
2023-10-19 13:47:23 -0700

    complete unit test for multiplication and concatOdds functions

---
+++ UnitTest.js
@@ -0,0 +1,26 @@
+/* 
+1. A function called "multiplication" that returns the product of the two input numbers.
+*/
+
+//Expect user input to be a number
+//Expect Funtion to multiply two numbers and return the product of both numbers 
+//Expect only two numbers no strings. Or it returns undefined
+//Expect multiplication(2,4) to be a number 
+//Expect multiplication(2,4) to be equal to 8
+//Expect multiplication("b", 2) to be an error 
+//Expect multiplication(2,4,2) to be an error 
+
+/*
+2. A function called "concatOdds" takes two arrays of integers as arguments. 
+    It should return a single array that only contains the odd numbers, in ascending order, 
+    from both of the arrays.
+*/
+
+//Expect concatOdds([], []) to take two arrays of numbers no strings or booleans
+//Expect concatOdds([], []) to concatenate both arrays into one array
+//Expect concatOdds([], []) to return one array of odd numbers only
+//Expect concatOdds([], []) to not return even numbers
+//Expect concatOdds([], []) to iterate through each number and return if it is odd from smallest to largest
+//Expect concatOdds([], []) to check if there is multiples of the same number and return that number just once
+//Expect concatOdds([3,"a","b"], [9,1,"c",1,4,15,-1]) to be an error 
+//Expect concatOdds([3,2,1], [9,1,1,1,4,15,-1]) to return [-1, 1, 3, 9, 15]
